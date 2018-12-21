b801c5dd843cac06f592cb5860ef00b9de0ffcf4
DionisisSchoinas <p17134@students.cs.unipi.gr>
2018-12-21 16:11:28 +0200

    Final Song Class

--- MusicPlaylist/MusicPlaylist/Song.cs
+++ MusicPlaylist/MusicPlaylist/Song.cs
@@ -8,15 +8,28 @@ namespace MusicPlaylist
 {
     class Song
     {
-        private int timesPlayed = 0;
-        private string songName = "", artistName = "", songLength = "", musicType = "", language = "";
-        private DateTime publishDate = new DateTime();
+        private int timesPlayed;
+        private string path, songName, artistName, musicType, language;
+        private DateTime publishDate;
+        private TimeSpan songLength;
         public int TimesPlayed { get { return timesPlayed; } set { timesPlayed = value; } }
+        public string Path { get { return path; } set { path = value; } }
         public string SongName { get { return songName; } set { songName = value; } }
         public string ArtistName { get { return artistName; } set { artistName = value; } }
-        public string SongLength { get { return songLength; } set { songLength = value; } }
+        public TimeSpan SongLength { get { return songLength; } set { songLength = value; } }
         public string MusicType { get { return musicType; } set { musicType = value; } }
         public string Language { get { return language; } set { language = value; } }
         public DateTime PublishDate { get { return publishDate; } set { publishDate = value; } }
+        Song()
+        {
+            timesPlayed = 0;
+            path = "";
+            songName = "";
+            artistName = "";
+            musicType = "";
+            language = "";
+            publishDate = new DateTime();
+            songLength = TimeSpan.FromSeconds(0);
+        }
     }
 }
